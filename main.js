74a467b611b54b83d302156e6fd6ffb8105ab6ca
unknown <dusanmihajlovic012@gmail.com>
2018-11-06 19:06:41 +0100

    charts progress

--- main.js
+++ main.js
@@ -52,51 +52,6 @@ $(function () {
         }
     })
 
-
-    var chart = $('.chart')
-    Highcharts.chart('container', {
-
-        chart: {
-            type: 'column'
-        },
-
-        title: {
-            text: 'HGw Status',
-            align: 'left',
-            x: 50,
-            y: 20
-        },
-
-        xAxis: {
-            categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
-        },
-
-        yAxis: {
-            allowDecimals: false,
-            min: 0,
-            title: {
-                text: 'Number of fruits'
-            }
-        },
-
-        series: [{
-            name: 'John',
-            data: [5, 3, 4, 7, 2],
-            stack: 'male'
-        }, {
-            name: 'Joe',
-            data: [3, 4, 4, 2, 5],
-            stack: 'male'
-        }, {
-            name: 'Jane',
-            data: [2, 5, 6, 2, 1],
-            stack: 'female'
-        }, {
-            name: 'Janet',
-            data: [3, 0, 4, 4, 3],
-            stack: 'female'
-        }]
-    });
-
+    
 
 })
