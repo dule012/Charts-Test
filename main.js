0fe91f66dcaab03292ece327dc3e35cc044a78b0
unknown <dusanmihajlovic012@gmail.com>
2018-11-07 03:09:22 +0100

    calendar responsive need functionality

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 $(function () {
     var arrOfMac = ['58-90-77-22-75-5C', '5A-3D-30-13-8F-15', 'A3-43-AE-41-2E-D7', 'B6-06-F6-C7-D5-04', '9D-01-4A-A4-CB-91', 'D7-C4-71-4A-47-35', '0D-D3-6B-05-86-59']
         , arrOfContract = ['54867972', '31495080', '31495080', '79238775', '39531833', '33992438', '93603004'],
+        arrOfMonths = ['Januar', 'February', 'March', 'April', 'May', 'June', 'Jul', 'August', 'September', 'October', 'November', 'December'],
+        arrOfDaysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    arrOfDays = ['Sunday', 'Monday', 'Tuesday', 'Wensday', 'Thursday', 'Friday', 'Saturday']
 
-        inputMac = $('input[name=mac]'),
+    firstJanuary = new Date('Jan 01 2018').getDay()
+    console.log(firstJanuary)
+
+    inputMac = $('input[name=mac]'),
         inputContract = $('input[name=contract]'),
         macWrapper = $('.mac-wrapper'),
         contractWrapper = $('.contract-wrapper')
@@ -52,6 +58,6 @@ $(function () {
         }
     })
 
-    
+
 
 })
